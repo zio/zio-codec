21e6308e6ae949f60a14bef261478c0018086485
sken <sken77@pm.me>
2020-04-20 18:39:58 -0700

    add more sensible tests

--- src/test/scala/zio/codec/CharCodecSpec.scala
+++ src/test/scala/zio/codec/CharCodecSpec.scala
@@ -1,9 +1,9 @@
 package zio.codec.examples
 
 import zio.Chunk
-import zio.codec.CharCodecModule
 import zio.test._
 import zio.test.Assertion._
+import zio.codec.{ CharCodecModule, Equiv }
 
 object CharCodecSpec extends DefaultRunnableSpec {
   object CharTest extends CharCodecModule
@@ -14,26 +14,48 @@ object CharCodecSpec extends DefaultRunnableSpec {
   def decodeSuccess[A](c: Codec[A], input: Chunk[Char])(expected: A) =
     assert(decoder(c)(input))(isRight(equalTo((input.size - 1, expected))))
 
-  def tokenAsSuccess[A](a: A) =
-    decodeSuccess(tokenAs(a.toString, a), toChunk(a.toString))(a)
-
-  case class Test(s: String, i: Int)
-
-  val genCase = for {
-    str <- Gen.string1(Gen.anyChar)
-    i   <- Gen.anyInt
-  } yield Test(str, i)
+  val genString1 = Gen.string1(Gen.anyChar)
 
   val spec = suite("CharCodecSpec")(
     testM("token")(
-      check(Gen.string1(Gen.anyChar))(str => decodeSuccess(token(str), toChunk(str))(toChunk(str)))
+      check(genString1)(str => decodeSuccess(token(str), toChunk(str))(toChunk(str)))
     ),
-    suite("tokenAs")(
-      testM("int")(check(Gen.anyInt)(tokenAsSuccess)),
-      testM("byte")(check(Gen.anyByte)(tokenAsSuccess)),
-      testM("string")(check(Gen.string1(Gen.anyChar))(tokenAsSuccess)),
-      testM("case class")(check(genCase)(tokenAsSuccess)),
-      testM("list")(check(Gen.listOf(Gen.anyInt))(tokenAsSuccess))
-    )
+    testM("token")(
+      check(genString1)(str => decodeSuccess(token(str), toChunk(str))(toChunk(str)))
+    ),
+    testM("tokenAs")(
+      check(Gen.anyInt)(a => decodeSuccess(tokenAs(a.toString, a), toChunk(a.toString))(a))
+    ),
+    testM("~")(
+      check(genString1)(str =>
+        decodeSuccess(token(str) ~ token(str.reverse), toChunk(str + str.reverse))((toChunk(str), toChunk(str.reverse)))
+      )
+    ),
+    testM("|")(
+      check(genString1)(str =>
+        decodeSuccess(token(str) | token(str.reverse), toChunk(str))(Left(toChunk(str))) &&
+          decodeSuccess(token(str) | token("noise" + str.reverse), toChunk("noise" + str.reverse))(
+            Right(toChunk("noise" + str.reverse))
+          )
+      )
+    ),
+    testM("rep")(
+      check(genString1, Gen.listOf1(Gen.string1(Gen.elements(' ')))) { (str, w) =>
+        val pattern = (token(str) ~ consume.oneOf(' ').rep).rep
+          .map(
+            Equiv.ForTesting(
+              _.fold[(Chunk[Input], Chunk[Input])]((Chunk.empty, Chunk.empty))((a, b) => (a._1 ++ b._1, a._2 ++ b._2))
+            )
+          )
+        decodeSuccess(pattern, toChunk(w.map(str + _).mkString))((toChunk(str * w.length), toChunk(w.mkString)))
+      }
+    ),
+    testM("repN") {
+      check(genString1, Gen.small(i => Gen.stringBounded(1, i)(Gen.elements(' ')), 1))((str, w) =>
+        decodeSuccess(token(str) ~ consume.oneOf(' ').repN(w.size), toChunk(str + w))(
+          (toChunk(str), toChunk(w.mkString))
+        )
+      )
+    }
   )
 }
