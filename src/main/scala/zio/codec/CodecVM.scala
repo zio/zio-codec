fff1810e796a99eeba8d0b3402adabc7f3fb3253
sergei-shabanau <serge.shabanau@gmail.com>
2020-05-19 10:12:48 -0400

    #26 - Use labels for jumps instead of addresses

--- src/main/scala/zio/codec/CodecVM.scala
+++ src/main/scala/zio/codec/CodecVM.scala
@@ -6,16 +6,16 @@ object CodecVM {
   private[zio] final case class Read(min: Option[Int], max: Option[Int])  extends CodecVM // read from input, push to stack
   private[zio] final case class Push(value: AnyRef)                       extends CodecVM // push value
   private[zio] final case class CheckSet(s: Set[Any])                     extends CodecVM // pot 1, test value is in set, push the result as boolean on the stack
-  private[zio] final case class Jump(to: Int)                             extends CodecVM // unconditional jump to new address
-  private[zio] final case class ICmpEq(address: Int)                      extends CodecVM // pop 2 integers and jump to new address if they are equal
-  private[zio] final case class ACmpEq(address: Int)                      extends CodecVM // pop 2 references and jump to new address if they are equal
-  private[zio] final case class ACmpNe(address: Int)                      extends CodecVM // pop 2 references and jump to new address if they are not equal
+  private[zio] final case class Jump(label: Int)                          extends CodecVM // unconditional jump to new address
+  private[zio] final case class ICmpEq(label: Int)                        extends CodecVM // pop 2 integers and jump to new address if they are equal
+  private[zio] final case class ACmpEq(label: Int)                        extends CodecVM // pop 2 references and jump to new address if they are equal
+  private[zio] final case class ACmpNe(label: Int)                        extends CodecVM // pop 2 references and jump to new address if they are not equal
   private[zio] final case class Construct1(f: AnyRef => AnyRef)           extends CodecVM // pop 1 thing, pass to f and push to stack
   private[zio] final case class Construct2(f: (AnyRef, AnyRef) => AnyRef) extends CodecVM // pop 2 things, pass to f and push to stack
   private[zio] final case class Fail(err: String)                         extends CodecVM // exit with failure
-  private[zio] final case class InvokeSame(name: Int)                     extends CodecVM // create a frame and continue
-  private[zio] final case class Invoke(name: Int, address: Int)           extends CodecVM // create a frame and jump to address
-  private[zio] final case class Return(name: Int)                         extends CodecVM // return from frame and jump to next instruction after invocation
+  private[zio] final case class BeginMethod(name: Int)                    extends CodecVM // denote start of a new method
+  private[zio] final case class Return(name: Int)                         extends CodecVM // return from method
+  private[zio] final case class InvokeStatic(name: Int, address: Int)     extends CodecVM // call a method
   private[zio] final case object Noop                                     extends CodecVM // do nothing
   private[zio] final case object Pop                                      extends CodecVM // pop 1 thing
   private[zio] final case object Duplicate                                extends CodecVM // duplicate stack head
